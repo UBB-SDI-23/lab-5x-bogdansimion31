033c96485abb1000492cd8c8999f014c70e6292c
bogdansimion31 <72395340+bogdansimion31@users.noreply.github.com>
2023-05-11 01:02:41 +0300

    fix

--- magazines/script.py
+++ magazines/script.py
@@ -103,93 +103,93 @@ fake = Faker()
 # print("Magazines generated")
 
 # Get the IDs of all Buyers
-# cur.execute("SELECT id FROM magazine_api_buyer")
-# buyer_ids = [row[0] for row in cur.fetchall()]
-#
-# # Generate and insert 1,000,000 BuyerSubscription records
-# for i in range(1000000):
-#     buyer_id = random.choice(buyer_ids)
-#     start_date = fake.date_between(start_date="-1y", end_date="today")
-#     end_date = fake.date_between(start_date=start_date, end_date="+1y")
-#
-#     # SQL query to insert a BuyerSubscription record
-#     query = "INSERT INTO magazine_api_buyersubscription (buyer_id, start_date, end_date) VALUES (%s, %s, %s)"
-#     values = (buyer_id, start_date, end_date)
-#
-#     # Execute the SQL query
-#     try:
-#         cur.execute(query, values)
-#     except psycopg2.errors.UniqueViolation:
-#         # Skip the insertion if a unique constraint violation error is thrown
-#         conn.rollback()
-#         continue
-#
-#     # Commit the changes to the database
-#     if i % 1000 == 0:
-#         conn.commit()
+cur.execute("SELECT id FROM magazine_api_buyer")
+buyer_ids = [row[0] for row in cur.fetchall()]
 
-# Get the IDs of all Authors and Publishers
-cur.execute("SELECT id FROM magazine_api_author")
-author_ids = [row[0] for row in cur.fetchall()]
-cur.execute("SELECT id FROM magazine_api_publisher")
-publisher_ids = [row[0] for row in cur.fetchall()]
-
-# Generate and insert 1,000 Magazine records
+# Generate and insert 1,000,000 BuyerSubscription records
 for i in range(1000000):
-    title = fake.sentence(nb_words=3)
-    number_of_pages = random.randint(1, 1000)
-    publish_date = fake.date_between(start_date="-10y", end_date="today")
-    quantity = random.randint(1, 100)
-    ibn = random.randint(10000, 99999)
-    author_id = random.choice(author_ids)
-    publisher_id = random.choice(publisher_ids)
-    price = random.randint(10, 100)
+    buyer_id = random.choice(buyer_ids)
+    start_date = fake.date_between(start_date="-1y", end_date="today")
+    end_date = fake.date_between(start_date=start_date, end_date="+1y")
 
-    # SQL query to insert a Magazine record
-    query = "INSERT INTO magazine_api_magazine (title, number_of_pages, publish_date, quantity, ibn, author_id, publisher_id, price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id"
-    values = (title, number_of_pages, publish_date, quantity, ibn, author_id, publisher_id, price)
+    # SQL query to insert a BuyerSubscription record
+    query = "INSERT INTO magazine_api_buyersubscription (buyer_id, start_date, end_date) VALUES (%s, %s, %s)"
+    values = (buyer_id, start_date, end_date)
 
+    # Execute the SQL query
     try:
-        # Execute the SQL query
         cur.execute(query, values)
+    except psycopg2.errors.UniqueViolation:
+        # Skip the insertion if a unique constraint violation error is thrown
+        conn.rollback()
+        continue
 
-        # Get the ID of the inserted Magazine record
-        magazine_id = cur.fetchone()[0]
-
-        # Generate and insert 1-5 Buyer records for the Magazine
-        for j in range(random.randint(1, 5)):
-            name = fake.name()
-            email = fake.email()
-            text = fake.sentence(nb_words=3)
-
-            # SQL query to insert a Buyer record
-            query = "INSERT INTO magazine_api_buyer (name, email, text) VALUES (%s, %s, %s) RETURNING id"
-            values = (name, email, text)
-
-            try:
-                # Execute the SQL query
-                cur.execute(query, values)
-
-                # Get the ID of the inserted Buyer record
-                buyer_id = cur.fetchone()[0]
-
-                # SQL query to insert a record into the MagazineBuyers table
-                query = "INSERT INTO magazine_api_magazine_buyers (magazine_id, buyer_id) VALUES (%s, %s)"
-                values = (magazine_id, buyer_id)
-
-                # Execute the SQL query
-                cur.execute(query, values)
-
-            except psycopg2.Error as e:
-                print(f"Error inserting Buyer record: {e}")
-                conn.rollback()
-
-        # Commit the changes to the database
+    # Commit the changes to the database
+    if i % 1000 == 0:
         conn.commit()
 
-    except psycopg2.Error as e:
-        print(f"Error inserting Magazine record: {e}")
-        conn.rollback()
+# Get the IDs of all Authors and Publishers
+# cur.execute("SELECT id FROM magazine_api_author")
+# author_ids = [row[0] for row in cur.fetchall()]
+# cur.execute("SELECT id FROM magazine_api_publisher")
+# publisher_ids = [row[0] for row in cur.fetchall()]
+#
+# Generate and insert 1,000 Magazine records
+# for i in range(1000000):
+#     title = fake.sentence(nb_words=3)
+#     number_of_pages = random.randint(1, 1000)
+#     publish_date = fake.date_between(start_date="-10y", end_date="today")
+#     quantity = random.randint(1, 100)
+#     ibn = random.randint(10000, 99999)
+#     author_id = random.choice(author_ids)
+#     publisher_id = random.choice(publisher_ids)
+#     price = random.randint(10, 100)
+#
+#     # SQL query to insert a Magazine record
+#     query = "INSERT INTO magazine_api_magazine (title, number_of_pages, publish_date, quantity, ibn, author_id, publisher_id, price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id"
+#     values = (title, number_of_pages, publish_date, quantity, ibn, author_id, publisher_id, price)
+#
+#     try:
+#         # Execute the SQL query
+#         cur.execute(query, values)
+#
+#         # Get the ID of the inserted Magazine record
+#         magazine_id = cur.fetchone()[0]
+#
+#         # Generate and insert 1-5 Buyer records for the Magazine
+#         for j in range(random.randint(1, 5)):
+#             name = fake.name()
+#             email = fake.email()
+#             text = fake.sentence(nb_words=3)
+#
+#             # SQL query to insert a Buyer record
+#             query = "INSERT INTO magazine_api_buyer (name, email, text) VALUES (%s, %s, %s) RETURNING id"
+#             values = (name, email, text)
+#
+#             try:
+#                 # Execute the SQL query
+#                 cur.execute(query, values)
+#
+#                 # Get the ID of the inserted Buyer record
+#                 buyer_id = cur.fetchone()[0]
+#
+#                 # SQL query to insert a record into the MagazineBuyers table
+#                 query = "INSERT INTO magazine_api_magazine_buyers (magazine_id, buyer_id) VALUES (%s, %s)"
+#                 values = (magazine_id, buyer_id)
+#
+#                 # Execute the SQL query
+#                 cur.execute(query, values)
+#
+#             except psycopg2.Error as e:
+#                 print(f"Error inserting Buyer record: {e}")
+#                 conn.rollback()
+#
+#         # Commit the changes to the database
+#         conn.commit()
+#
+#     except psycopg2.Error as e:
+#         print(f"Error inserting Magazine record: {e}")
+#         conn.rollback()
 
 cur.execute("SELECT id FROM magazine_api_magazine")
 magazines = cur.fetchall()
